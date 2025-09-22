1471b7be3cda1984dff466a3453648c1586057b3
Sarthak Pawar <sarthakjob2604@gmail.com>
2025-09-22 21:30:52 +0530

    done a loop for first name find the similar letters from word1 and word2

--- love_cal.py
+++ love_cal.py
@@ -3,3 +3,9 @@ def calculate_love_score(name1,name2):
     word2 = "Love"
     count = 0
     count2 = 0
+
+    for item in name1.upper():
+        if item in word1:
+            count+=1
+        if item in word2:
+            count2+= 1
