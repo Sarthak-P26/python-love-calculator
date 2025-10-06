6d3c120e76fc275d99360af2e804cf078e15b9e0
Sarthak Pawar <sarthakjob2604@gmail.com>
2025-10-06 23:19:40 +0530

    added emojis to the code

--- love_cal.py
+++ love_cal.py
@@ -1,29 +1,30 @@
-def calculate_love_score(name1,name2):
+def calculate_love_score(name1, name2):
     word1 = "TRUE"
-    word2 = "Love"
+    word2 = "LOVE"
     count = 0
     count2 = 0
 
     for item in name1.upper():
         if item in word1:
-            count+=1
+            count += 1
         if item in word2:
-            count2+= 1
+            count2 += 1
 
     for item in name2.upper():
         if item in word1:
-            count+= 1
+            count += 1
         if item in word2:
-            count2+= 1
+            count2 += 1
 
     love_score = str(count) + str(count2)
-    print(love_score)
+    print("💖 Your Love Score is:", love_score, "💖")
 
-print("Welcome to love Calculator....")
-print("Let's calculate your score 💖")
 
-name1 = input("Enter your first name: ")
-name2 = input("Enter your partner's name: ")
+print("💞 Welcome to Love Calculator 💞")
+print("Let's calculate your score 💘")
 
-print("This is your love score: ", end = " ")
+name1 = input("✨ Enter your first name: ")
+name2 = input("💕 Enter your partner's name: ")
+
+print("🔮 This is your love score: ", end=" ")
 calculate_love_score(name1, name2)
